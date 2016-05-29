3b28041f7ab8d16d92d21e4c4a2c02d54770a246
revit <revit@172.19.3.212>
2016-05-29 15:52:43 +0300

    icon in maze

--- Web03/web/Maze.js
+++ Web03/web/Maze.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-function generate_table(mazeString, size, startRow, startCol, endRow, endCol) {
+function generate_table(mazeString, size, startRow, startCol, endRow, endCol, icon) {
     // get the reference for the body
     var body=document.getElementsByClassName("Main")[0];
    // var body = document.getElementsByTagName("body")[0];
@@ -29,11 +29,14 @@ function generate_table(mazeString, size, startRow, startCol, endRow, endCol) {
             //cell.style.borderSpacing = "0px";
             row.appendChild(cell);
             if (mazeString.charAt(x) === '1') {
-                cell.style.background = "pink";
+                cell.style.background = "purple";
             }
             if ((i === (parseInt(startRow) *2) && j === (parseInt(startCol) * 2))) {
                // var u = session.getAttribute("Curr");
-              //  cell.style.backgroundImage = "url('u.icon')";
+            //   cell.style.background = "yellow";
+            var str = "url(" + icon + ")";
+               cell.style.backgroundImage = str;
+                cell.style.backgroundSize = "cover";
             } 
             if (i === (parseInt(endRow) * 2) && j === (parseInt(endCol) * 2)) {
                 cell.style.backgroundImage = "url('/../pic/yellowflower.jpg')";
@@ -48,7 +51,7 @@ function generate_table(mazeString, size, startRow, startCol, endRow, endCol) {
 
     // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
-    tbl.style.background="purple";
+    tbl.style.background="pink";
     tbl.style.borderSpacing="0px";
     tbl.style.width = "500px";
     tbl.style.height = "500px";
