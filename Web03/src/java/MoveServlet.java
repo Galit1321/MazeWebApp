e236d1f454583854318f99340a2c03beae5b223e
galit1321 <galit1321@gmail.com>
2016-05-30 15:46:14 +0300

    down works

--- Web03/src/java/MoveServlet.java
+++ Web03/src/java/MoveServlet.java
@@ -27,8 +27,8 @@ import org.json.JSONObject;
  */
 @WebServlet(urlPatterns = {"/MoveServlet"})
 public class MoveServlet extends HttpServlet {
-    private Model m;
 
+    private Model m;
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -42,7 +42,7 @@ public class MoveServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      //  processRequest(request, response);
+        //  processRequest(request, response);
     }
 
     /**
@@ -56,41 +56,45 @@ public class MoveServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       // processRequest(request, response);
+        // processRequest(request, response);
         HttpSession session = request.getSession(false);
         User u = (User) session.getAttribute("Curr");
         this.m = u.mode;
-            Pair p = u.myMaze.move(request.getParameter("move"));
-            if (u.myMaze.getCurrrnt().equals(p))//we didnt move at all 
-            {
-                return;
+        Pair p = u.getMaze().move(request.getParameter("move"));
+        JSONObject obj = new JSONObject();
+     /*  if (u.getMaze().getCurrrnt().equals(p))//we didnt move at all 
+        {
+           int id = -1;
+            try {
+                obj.put("location", id);
+            } catch (JSONException ex) {
+                Logger.getLogger(MoveServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-            int myRow = (int) p.getKey();
-            int myCol = (int) p.getValue();
-            u.myMaze.setCurrent(p);
-            //Position in the string.
-            int pos = (u.myMaze.getSize() * myRow) + myRow;
-           // if (MyMaze.solv[pos].Equals('2'))
-           // {
+        }*/
+        int myRow = (int) p.getKey();
+        int myCol = (int) p.getValue();
+        u.getMaze().setCurrent(p);
+        //Position in the string.
+        int pos = (u.getMaze().getSize() * myRow) + myRow;
+        int id = (myRow * 10) + myCol;
+        try {
+            obj.put("location", id);
+            // if (MyMaze.solv[pos].Equals('2'))
+            // {
             //    MyMaze.lastClue.Add(pos);
             //}
-           //     if ((this.Coordinate.Equals(MyMaze.End)))//we reach goal in maze;
+            //     if ((this.Coordinate.Equals(MyMaze.End)))//we reach goal in maze;
             //{
-              //  Winner = true;
-           // }
-        JSONObject obj = new JSONObject();
-        if (m.getJson() != null) {
-            try {
-                int id = (myRow * 10) + myCol;
-                obj.put("location", id);
-               // this.sendrq = false;
-               // counter = 100;
-            } catch (JSONException ex) {
-                Logger.getLogger(ProgressServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            //  Winner = true;
+            // }
+        } catch (JSONException ex) {
+            Logger.getLogger(MoveServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.setContentType("appliation/json");
+        try (PrintWriter out = response.getWriter()) {
+            out.println(obj);
+        }
+        
     }
-    }
-    
 
-   
 }
